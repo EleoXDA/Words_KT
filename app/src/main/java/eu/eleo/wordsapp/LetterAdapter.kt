f89dbfec304bcfdb5c09bc6c5870b03e600a6631
Elbay Malik <eleo.666@gmail.com>
2022-02-15 13:43:28 +0100

    Adding listener to letter change

--- app/src/main/java/eu/eleo/wordsapp/LetterAdapter.kt
+++ app/src/main/java/eu/eleo/wordsapp/LetterAdapter.kt
@@ -15,6 +15,7 @@
  */
 package eu.eleo.wordsapp
 
+import android.content.Intent
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
@@ -52,7 +53,7 @@ class LetterAdapter :
                 .from(parent.context)
                 .inflate(R.layout.item_view, parent, false)
         // Setup custom accessibility delegate to set the text read
-        layout.accessibilityDelegate = Accessibility
+        layout.accessibilityDelegate = WordAdapter
         return LetterViewHolder(layout)
     }
 
@@ -62,6 +63,12 @@ class LetterAdapter :
     override fun onBindViewHolder(holder: LetterViewHolder, position: Int) {
         val item = list.get(position)
         holder.button.text = item.toString()
+        holder.button.setOnClickListener {
+        val context = holder.view.context
+        val intent = Intent(context, DetailActivity::class.java)
+        intent.putExtra("letter", holder.button.text.toString())
+        context.startActivity(intent)
+        }
     }
 
     // Setup custom accessibility delegate to set the text read with
