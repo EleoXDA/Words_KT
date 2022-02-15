221725fe950d934cc18304569feb7355e7863caf
Elbay Malik <eleo.666@gmail.com>
2022-02-15 13:44:48 +0100

    Changing letter choice from A to "any"

--- app/src/main/java/eu/eleo/wordsapp/DetailActivity.kt
+++ app/src/main/java/eu/eleo/wordsapp/DetailActivity.kt
@@ -36,7 +36,7 @@ class DetailActivity : AppCompatActivity() {
         // Retrieve the LETTER from the Intent extras
         // intent.extras.getString returns String? (String or null)
         // so toString() guarantees that the value will be a String
-        val letterId = "A"
+        val letterId = intent?.extras?.getString("letter").toString()
 
         val recyclerView = binding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(this)
