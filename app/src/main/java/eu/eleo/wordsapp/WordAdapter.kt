0299b398f8dcbe5d40de9898cafd90537b4c909a
Elbay Malik <eleo.666@gmail.com>
2022-02-15 19:52:26 +0100

    Adapting same into WordAdapter.kt

--- app/src/main/java/eu/eleo/wordsapp/WordAdapter.kt
+++ app/src/main/java/eu/eleo/wordsapp/WordAdapter.kt
@@ -16,6 +16,8 @@
 package eu.eleo.wordsapp
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
@@ -81,7 +83,11 @@ class WordAdapter(private val letterId: String, context: Context) :
 
         // Set the text of the WordViewHolder
         holder.button.text = item
-
+        holder.button.setOnClickListener{
+            val queryURL: Uri = Uri.parse("${DetailActivity.SEARCH_PREFIX}${item}")
+            val intent = Intent(Intent.ACTION_VIEW, queryURL)
+            context.startActivity(intent)
+        }
     }
     // Setup custom accessibility delegate to set the text read with
     // an accessibility service
