8c6d03953ca5911b0d839b60cb12d9d69466e69f
Elbay Malik <eleo.666@gmail.com>
2022-02-17 23:14:34 +0100

    Use fragment kotlin file to adapt for the fragment life cycle

--- app/src/main/java/eu/eleo/wordsapp/WordListFragment.kt
+++ app/src/main/java/eu/eleo/wordsapp/WordListFragment.kt
@@ -1,7 +1,40 @@
 package eu.eleo.wordsapp
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import eu.eleo.wordsapp.databinding.FragmentWordListBinding
 
 class WordListFragment : Fragment() {
+    companion object{
+        const val LETTER = "letter"
+        const val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
 
+    private var _binding: FragmentWordListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        _binding = FragmentWordListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = WordAdapter(activity?.intent?.extras?.getString(LETTER).toString(),
+            requireContext())
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        )
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
