b45ee04247e453ec40f08c251c5849d1b370027c
Elbay Malik <eleo.666@gmail.com>
2022-02-17 23:10:24 +0100

    LetterAdapter.kt update to change click listener to use WordListFragment.kt

--- app/src/main/java/eu/eleo/wordsapp/LetterAdapter.kt
+++ app/src/main/java/eu/eleo/wordsapp/LetterAdapter.kt
@@ -38,7 +38,7 @@ class LetterAdapter :
      * Provides a reference for the views needed to display items in your list.
      */
     class LetterViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        val button = view.findViewById<Button>(R.id.button_item)
+        val button: Button = view.findViewById<Button>(R.id.button_item)
     }
 
     override fun getItemCount(): Int {
@@ -66,8 +66,7 @@ class LetterAdapter :
         holder.button.setOnClickListener {
         val context = holder.view.context
         val intent = Intent(context, DetailActivity::class.java)
-        //intent.putExtra("letter", holder.button.text.toString())
-            intent.putExtra(DetailActivity.LETTER, holder.button.text.toString())
+            intent.putExtra(WordListFragment.LETTER, holder.button.text.toString())
             context.startActivity(intent)
         }
     }
