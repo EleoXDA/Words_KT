54529a48a3d133b2d79d96a344d7d62390b720e5
Elbay Malik <eleo.666@gmail.com>
2022-02-18 16:53:41 +0100

    Navigation tests to check app functions

--- app/src/androidTest/java/eu/eleo/wordsapp/NavigationTests.kt
+++ app/src/androidTest/java/eu/eleo/wordsapp/NavigationTests.kt
@@ -1,8 +1,16 @@
 package eu.eleo.wordsapp
 
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.runner.AndroidJUnit4
+import junit.framework.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -18,6 +26,16 @@ class NavigationTests {
             themeResId =
             R.style.Theme_Words
         )
-    }
+        letterListScenario.onFragment { fragment ->
+            navController.setGraph(R.navigation.nav_graph)
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
 
+        onView(withId(R.id.recycler_view))
+            .perform(
+                RecyclerViewActions
+                    .actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click())
+            )
+        assertEquals(navController.currentDestination?.id, R.id.wordListFragment)
+    }
 }
