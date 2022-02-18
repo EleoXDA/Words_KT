312d9c9fa7a9634d6096cb0aad834368fde18115
Elbay Malik <eleo.666@gmail.com>
2022-02-18 12:41:09 +0100

    Fix for implementing navigation in WordListFragment.kt

--- app/src/main/java/eu/eleo/wordsapp/WordListFragment.kt
+++ app/src/main/java/eu/eleo/wordsapp/WordListFragment.kt
@@ -17,6 +17,15 @@ class WordListFragment : Fragment() {
 
     private var _binding: FragmentWordListBinding? = null
     private val binding get() = _binding!!
+    private lateinit var letterId: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            letterId = it.getString(LETTER).toString()
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
@@ -27,8 +36,7 @@ class WordListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val recyclerView = binding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        recyclerView.adapter = WordAdapter(activity?.intent?.extras?.getString(LETTER).toString(),
-            requireContext())
+        recyclerView.adapter = WordAdapter(letterId, requireContext())
         recyclerView.addItemDecoration(
             DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
         )
@@ -37,4 +45,5 @@ class WordListFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
 }
